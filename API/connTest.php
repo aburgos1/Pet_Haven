c6880df1e732a5142cf35c221218c3be22718070
aburgos1 <angel.burgos1@upr.edu>
2016-11-17 11:50:19 -0400

    Testing connection to online server
    Test was succesful.

---
+++ API/connTest.php
@@ -0,0 +1,14 @@
+<?php
+$servername = "a2ss23.a2hosting.com";
+$username = "pethaven_user";
+$password = "havenpet.";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+echo "Connected successfully";
+?>
