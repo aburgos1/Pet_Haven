9d279859e3c00e58940a6f3dc1e88e035f4aa746
aburgos1 <angel.burgos1@upr.edu>
2016-11-18 11:28:03 -0400

    Gets all the dogs
    URL: www.pethavenpr.com/GetDogs.php

---
+++ API/GetDogs.php
@@ -0,0 +1,35 @@
+<?php
+
+$servername = "a2ss23.a2hosting.com";
+$username = "pethaven_user";
+$password = "havenpet.";
+$dbname = "pethaven_data";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+//echo "Connected successfully" . "<br>";
+
+$sql = "SELECT animal_ID, animal_name, species FROM animal WHERE species = 'dog' AND adopted = 0";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+     
+    $resultarr = array();
+
+    while($row = $result->fetch_row()) {
+              $resultarr[] = $row;
+    }
+
+    echo json_encode($resultarr);
+        
+
+} else {
+    echo "0 results";
+}
+$conn->close();
+?>
