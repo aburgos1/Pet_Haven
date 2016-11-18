551f3ebf37c2db8be350f967db868383332e4236
aburgos1 <angel.burgos1@upr.edu>
2016-11-18 12:04:15 -0400

    Gets the count of all the animals in the database
    URL: www.pethavenpr.com/AnimalCount.php

---
+++ API/AnimalCount.php
@@ -0,0 +1,30 @@
+<?php
+
+$servername = "a2ss23.a2hosting.com";
+$username = "pethaven_user";
+$password = "havenpet.";
+$dbname = "pethaven_data";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+//echo "Connected successfully" . "<br>";
+
+$sql = "SELECT COUNT(*) FROM animal";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+     
+   $count = $result->fetch_row();  
+   echo $count[0];
+        
+
+} else {
+    echo "0 results";
+}
+$conn->close();
+?>
