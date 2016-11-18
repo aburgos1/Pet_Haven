3068eec374b7ea242a21c777ab0a0e3bbbd288ea
aburgos1 <angel.burgos1@upr.edu>
2016-11-18 17:35:29 -0400

    Gets all forms submitted by user
    URL: http://www.pethavenpr.com/GetUserForms.php?id="user_ID" . "user_ID" is the ID number of the user.

---
+++ API/GetUserForms.php
@@ -0,0 +1,38 @@
+<?php
+
+$servername = "a2ss23.a2hosting.com";
+$username = "pethaven_user";
+$password = "havenpet.";
+$dbname = "pethaven_data";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+//echo "Connected successfully" . "<br>";
+
+$id = (isset($_GET["id"]) ? $_GET["id"] : '');
+
+$sql = "SELECT form_ID, user_ID, email, pnum, purpose, date_submitted FROM form WHERE user_ID = " . mysqli_real_escape_string($conn,$id);
+
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+     
+    $resultarr = array();
+
+    while($row = $result->fetch_row()) {
+              $resultarr[] = $row;
+    }
+
+    echo json_encode($resultarr);
+        
+
+} else {
+    echo "0 results";
+}
+$conn->close();
+?>
