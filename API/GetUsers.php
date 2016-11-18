10cb3af73b6b61c8abd09cb7fc82700222bd646c
aburgos1 <angel.burgos1@upr.edu>
2016-11-18 11:39:17 -0400

    Gets all the users in the database.
    URL: www.pethavenpr.com/GetUsers.php

---
+++ API/GetUsers.php
@@ -0,0 +1,35 @@
+<?php
+
+$servername = "a2ss23.a2hosting.com";
+$username = "pethaven_user";
+$password = "havenpet.";
+$dbname = "pethaven_data";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+//echo "Connected successfully" . "<br>";
+
+$sql = "SELECT user_ID, email, first_name, last_name, is_admin FROM user";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+     
+    $resultarr = array();
+
+    while($row = $result->fetch_row()) {
+              $resultarr[] = $row;
+    }
+
+    echo json_encode($resultarr);
+        
+
+} else {
+    echo "0 results";
+}
+$conn->close();
+?>
