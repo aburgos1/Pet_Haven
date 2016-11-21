7db0d4e7d43f37d0411fe345648e1872062f4fea
aburgos1 <angel.burgos1@upr.edu>
2016-11-21 19:21:45 -0400

    Gets an user based on its user ID
    URL: www.pethavenpr.com/GetUser.php?uID=user_ID

---
+++ API/GetUser.php
@@ -0,0 +1,38 @@
+<?php
+
+$servername = "a2ss23.a2hosting.com";
+$username = "pethaven_user";
+$password = "havenpet.";
+$dbname = "pethaven_data";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+//echo "Connected successfully" . "<br>";
+
+$uID = (isset($_GET["uID"]) ? $_GET["uID"] : '');
+
+$sql = "SELECT first_name, last_name, email, pnum, address FROM user WHERE user_ID = " . mysqli_real_escape_string($conn,$uID);
+
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+     
+    $resultarr = array();
+
+    while($row = $result->fetch_row()) {
+              $resultarr[] = $row;
+    }
+
+    echo json_encode($resultarr);
+        
+
+} else {
+    echo "0 results";
+}
+$conn->close();
+?>
