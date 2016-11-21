ff1b4bf8c32f407eca105f7bfdf456abab01022e
aburgos1 <angel.burgos1@upr.edu>
2016-11-21 16:37:16 -0400

    Sets an animal as Ready to Adopt
    URL: www.pethavenpr.com/SetReadyToAdopt.php?aID=animal_ID

---
+++ API/SetReadyToAdopt.php
@@ -0,0 +1,29 @@
+<?php
+
+$servername = "a2ss23.a2hosting.com";
+$username = "pethaven_user";
+$password = "havenpet.";
+$dbname = "pethaven_data";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+//echo "Connected successfully" . "<br>";
+$aID = (isset($_GET["aID"]) ? $_GET["aID"] : '');
+
+
+$sql = "UPDATE animal SET ready_to_adopt = 1 WHERE animal_ID = " . mysqli_real_escape_string($conn,$aID);
+
+
+if (mysqli_query($conn, $sql)) {
+    echo "Record deleted successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+
+$conn->close();
+?>
