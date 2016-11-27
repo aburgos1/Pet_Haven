de7e12cc6f6cb37d1a68659b83c1e3cf9079b624
aburgos1 <angel.burgos1@upr.edu>
2016-11-27 18:47:27 -0400

    Gets picture of an animal

---
+++ API/GetAnimalPic.php
@@ -0,0 +1,30 @@
+<?php
+
+$servername = "a2ss23.a2hosting.com";
+$username = "pethaven_user";
+$password = "havenpet.";
+$dbname = "pethaven_data";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+//echo "Connected successfully" . "<br>";
+
+
+$aID = (isset($_GET["aID"]) ? $_GET["aID"] : '');
+
+$sql = "SELECT pic FROM animal WHERE animal_ID = " . mysqli_real_escape_string($conn,$aID);
+
+$result = $conn->query($sql);
+
+$img = mysqli_fetch_array($result, MYSQLI_BOTH);
+
+echo '<img src ="data:image/jpeg;base64,'.base64_encode($img['pic']).'"/>';
+
+
+$conn->close();
+?>
