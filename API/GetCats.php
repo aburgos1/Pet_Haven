d82c33a57734efe018212c1df2bf66cc5d30c9e8
aburgos1 <angel.burgos1@upr.edu>
2016-11-27 19:04:11 -0400

    Update GetCats.php

--- API/GetCats.php
+++ API/GetCats.php
@@ -14,7 +14,7 @@ if ($conn->connect_error) {
 } 
 //echo "Connected successfully" . "<br>";
 
-$sql = "SELECT animal_ID, animal_name, species FROM animal WHERE species = 'cat' AND adopted = 0";
+$sql = "SELECT animal_ID, animal_name, gender, size, species FROM animal WHERE species = 'cat' AND adopted = 0";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
