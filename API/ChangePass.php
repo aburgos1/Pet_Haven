fec450298993bfb26d587df35364436a2f862ec3
aburgos1 <angel.burgos1@upr.edu>
2016-11-27 18:22:48 -0400

    Added comments

--- API/ChangePass.php
+++ API/ChangePass.php
@@ -1,5 +1,6 @@
 <?php
-
+//Changes the password stored in the database. URL: www.pethavenpr.com/ChangePass.php?uID=user_ID&pass='newPass' 
+//where user_ID is the ID of the user currently logged in and newPass is the new password.
 $servername = "a2ss23.a2hosting.com";
 $username = "pethaven_user";
 $password = "havenpet.";
@@ -16,9 +17,9 @@ if ($conn->connect_error) {
 //echo "Connected successfully" . "<br>";
 $uID = (isset($_GET["uID"]) ? $_GET["uID"] : '');
 $newPass = (isset($_GET["pass"]) ? $_GET["pass"] : '');
-
+//Hashes the password entered.
 $hashed = crypt($newPass,$salt);
-
+//Stores the hashed password in the database.
 $sql = "UPDATE user SET pass = '". $hashed . "' WHERE user_ID = " . mysqli_real_escape_string($conn,$uID);
 
 if (mysqli_query($conn, $sql)) {
