09ad5d06b8dc379b3be654a2c82d6a589360b539
aburgos1 <angel.burgos1@upr.edu>
2016-11-30 22:27:54 -0400

    Added comments

--- API/SetNotAdopted.php
+++ API/SetNotAdopted.php
@@ -1,5 +1,6 @@
 <?php
-
+//Sets an animal as not adopted. URL: www.pethavenpr.com?SetNotAdopted.php?aID=animal_ID
+//Where animal_ID is the ID of the animal to be set as not adopted.
 $servername = "a2ss23.a2hosting.com";
 $username = "pethaven_user";
 $password = "havenpet.";
